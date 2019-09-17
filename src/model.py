9d5d47f79ded12d5ff0b0d45d2a0ac4bb797335e
RuihongQiu_gs522-6979 <qiuruihong@foxmail.com>
2019-09-17 15:35:41 +1000

    Variable naming.

--- src/model.py
+++ src/model.py
@@ -20,14 +20,14 @@ class Embedding2Score(nn.Module):
         self.q = nn.Linear(self.hidden_size, 1)
         self.W_3 = nn.Linear(2 * self.hidden_size, self.hidden_size)
 
-    def forward(self, session_embedding, item_embedding_table, batch, num_count):
+    def forward(self, node_embedding, item_embedding_table, batch, num_count):
         sections = torch.bincount(batch)
-        v_i = torch.split(session_embedding, tuple(sections.cpu().numpy()))    # split whole x back into graphs G_i
+        v_i = torch.split(node_embedding, tuple(sections.cpu().numpy()))    # split whole x back into graphs G_i
         v_n_repeat = tuple(nodes[-1].view(1, -1).repeat(nodes.shape[0], 1) for nodes in v_i)    # repeat |V|_i times for the last node embedding
 
         # Eq(6)
-        alpha = self.q(torch.sigmoid(self.W_1(torch.cat(v_n_repeat, dim=0)) + self.W_2(session_embedding)))    # |V|_i * 1
-        s_g_whole = num_count.view(-1, 1) * alpha * session_embedding    # |V|_i * hidden_size
+        alpha = self.q(torch.sigmoid(self.W_1(torch.cat(v_n_repeat, dim=0)) + self.W_2(node_embedding)))    # |V|_i * 1
+        s_g_whole = num_count.view(-1, 1) * alpha * node_embedding    # |V|_i * hidden_size
         s_g_split = torch.split(s_g_whole, tuple(sections.cpu().numpy()))    # split whole s_g into graphs G_i
         s_g = tuple(torch.sum(embeddings, dim=0).view(1, -1) for embeddings in s_g_split)
 
